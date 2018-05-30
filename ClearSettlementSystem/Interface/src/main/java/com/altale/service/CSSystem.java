4cc3f555718472600e2f471715b84451372b068d
Tsuioku <578340750@qq.com>
2018-05-30 22:19:49 +0800

    调整接口（没做完）

--- ClearSettlementSystem/Interface/src/main/java/com/altale/service/CSSystem.java
+++ ClearSettlementSystem/Interface/src/main/java/com/altale/service/CSSystem.java
@@ -8,24 +8,35 @@ public interface CSSystem {
      * @param userID 用户ID
      * @param amount 充值金额
      * @param method 充值方式：false-微信，true-支付宝
-     * @param requestTime 请求时间
+     * @param requestTime 请求时间 "YY-MM-DD HH:MM:SS"
      * @return 返回请求ID，若失败返回"-1"
      * @throws RequestException
      */
     public String Recharge(String requestID, String userID, double amount, boolean method, String requestTime) throws RequestException;
 
     /**
-     *
-     * @param requestID
-     * @param userID
-     * @param amount
-     * @param method
-     * @param requestTime
-     * @return
+     *提现接口
+     * @param requestID 请求ID
+     * @param userID 用户ID
+     * @param amount 提现金额
+     * @param method 提现方式：false-微信，true-支付宝
+     * @param requestTime 请求时间 "YY-MM-DD HH:MM:SS"
+     * @return 返回请求ID，若失败返回"-1"
      * @throws RequestException
      */
     public String Withdraw(String requestID, String userID, double amount, boolean method, String requestTime) throws RequestException;
-    //交易接口
+    
+    /**
+	 * 交易接口
+	 * @param requestID 请求ID
+	 * @param userID 用户ID
+	 * @param merchantID 商家ID
+	 * @param amount 交易金额
+	 * @param operateStatus 操作状态
+	 * @param requestTime 请求时间 "YY-MM-DD HH:MM:SS"
+     * @return 返回请求ID，若失败返回"-1"
+	 * @throws RequestException
+	 */
     public String Trade(String requestID, String userID, String merchantID, double amount, boolean operateStatus, String requestTime) throws RequestException;
     //查询记录接口
     public String QueryRecord(String startTime,String endTime,int kind) throws TimeOutOfRangeException,OperatorIdOutOfRangeException;
