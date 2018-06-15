754bdc6f9e5c1db1d4c3bf770de30fa116222a8d
altale <578340750@qq.com>
2018-06-15 12:17:05 +0800

    对账文件生成测试类

---
+++ ClearSettlementSystem/Provider/src/test/java/com/altale/service/impl/CreateTestFileUtil.java
@@ -0,0 +1,31 @@
+package com.altale.service.impl;
+
+import com.altale.util.Constant;
+import com.altale.util.DateUtil;
+
+import java.io.File;
+import java.util.Date;
+
+public class CreateTestFileUtil {
+    public static void main(String args[]) {
+        Date now = new Date();
+        Date lim = DateUtil.toDayBefore(now, 15);
+        File dir=new File(Constant.jspath);
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+        while (lim.before(now)) {
+            File file = new File(Constant.jspath + DateUtil.dateToString(lim, 1) + ".json");
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            System.out.println(file.getAbsolutePath()+" "+now.toString());
+            lim=DateUtil.toDayBefore(lim,-1);
+        }
+    }
+}
