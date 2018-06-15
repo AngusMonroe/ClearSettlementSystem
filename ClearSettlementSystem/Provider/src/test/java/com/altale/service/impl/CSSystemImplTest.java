5e8db1b6705f5379981af3131c80c51b668816dc
xiongbarton <38251885+xiongbarton@users.noreply.github.com>
2018-06-15 11:35:43 +0800

    更新测试文档，上传测试类
    时间BUG暂未解决

---
+++ ClearSettlementSystem/Provider/src/test/java/com/altale/service/impl/CSSystemImplTest.java
@@ -0,0 +1,440 @@
+package com.altale.service.impl;
+
+import com.altale.Launcher;
+import com.altale.service.connection.CSSdbinfo;
+import com.altale.service.connection.SQLConnection;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CSSystemImplTest {
+
+    @Before
+    public void bef1()
+    {
+        try
+        {
+            Launcher.sqlConnection = new SQLConnection
+                    (
+                            "jdbc:mysql://" +
+                                    CSSdbinfo.server +  ":" +
+                                    CSSdbinfo.port + "/" +
+                                    "test" + "?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=GMT%2B8",
+                            "root",
+                            ""
+                    );
+
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /*recharge*/
+/*    @Test
+    public void recharge1() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("","1621",233,true,"2018-06-06 12:00:30"));
+    }//requestID为空
+    @Test
+    public void recharge2() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("abc","1621",233,true,"2018-06-06 12:00:30"));
+    }//requestID错误
+    @Test
+    public void recharge3() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("23456","",233,true,"2018-06-06 12:00:30"));
+    }//userID为空
+    @Test
+    public void recharge4() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("23456","abc",233,true,"2018-06-06 12:00:30"));
+    }//userID错误
+    @Test
+    public void recharge5() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("23456","1621",-100,true,"2018-06-06 12:00:30"));
+    }//amount为负
+    @Test
+    public void recharge6() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("104","1621",21,true,"2018-00-01 12:00:30"));
+    }//月份错误1
+    @Test
+    public void recharge7() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("1001","1621",21,true,"2018-13-01 12:00:30"));
+    }//月份错误2
+    @Test
+    public void recharge8() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("1001","1621",21,true,"2018-05-32 12:00:30"));
+    }//大月错误
+    @Test
+    public void recharge9() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("1001","1621",21,true,"2018-06-31 12:00:30"));
+    }//小月错误
+    @Test
+    public void recharge10() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("1001","1621",21,true,"2018-02-29 12:00:30"));
+    }//非闰年2月
+    @Test
+    public void recharge11() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("1001","1621",21,true,"2008-02-30 12:00:30"));
+    }//闰年2月
+    @Test
+    public void recharge12() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("1001","1621",21,true,"-2018-06-06-"));
+    }//时间格式有误
+    @Test
+    public void recharge13() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("1001","1621",21,true,"2018-06-06 24:00:00"));
+    }//小时错误
+    @Test
+    public void recharge14() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("1001","1621",21,true,"2018-06-06 12:60:00"));
+    }//分错误
+    @Test
+    public void recharge15() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("1001","1621",21,true,"2018-06-06 12:00:60"));
+    }//秒错误
+    @Test
+    public void recharge16() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Recharge("1001","1621",21,true,""));
+    }//时间空
+
+
+    //正确参数
+    @Test
+    public void rechargeA() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("1001",
+                css.Recharge("1001","1621",21,true,"2018-05-30 00:00:00"));
+    }
+    @Test
+    public void rechargeB() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("1002",
+                css.Recharge("1002","1621",21,true,"2018-06-30 00:00:00"));
+    }
+    @Test
+    public void rechargeC() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("1003",
+                css.Recharge("1003","1621",21,true,"2018-02-28 00:00:00"));
+    }
+    @Test
+    public void rechargeD() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("1004",
+                css.Recharge("1004","1621",21,true,"2008-02-29 00:00:00"));
+    }
+
+*/
+/*withdraw*/
+/*
+    @Test
+    public void withdraw1() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("","1621",233,true,"2018-06-06 12:00:30"));
+    }//requestID为空
+    @Test
+    public void withdraw2() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("abc","1621",233,true,"2018-06-06 12:00:30"));
+    }//requestID错误
+    @Test
+    public void withdraw3() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("23456","",233,true,"2018-06-06 12:00:30"));
+    }//userID为空
+    @Test
+    public void withdraw4() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("23456","abc",233,true,"2018-06-06 12:00:30"));
+    }//userID错误
+    @Test
+    public void withdraw5() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("23456","1621",-100,true,"2018-06-06 12:00:30"));
+    }//amount为负
+    @Test
+    public void withdraw6() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("104","1621",21,true,"2018-00-01 12:00:30"));
+    }//月份错误1
+    @Test
+    public void withdraw7() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("1001","1621",21,true,"2018-13-01 12:00:30"));
+    }//月份错误2
+    @Test
+    public void withdraw8() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("1001","1621",21,true,"2018-05-32 12:00:30"));
+    }//大月错误
+    @Test
+    public void withdraw9() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("1001","1621",21,true,"2018-06-31 12:00:30"));
+    }//小月错误
+    @Test
+    public void withdraw10() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("1001","1621",21,true,"2018-02-29 12:00:30"));
+    }//非闰年2月
+    @Test
+    public void withdraw11() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("1001","1621",21,true,"2008-02-30 12:00:30"));
+    }//闰年2月
+    @Test
+    public void withdraw12() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("1001","1621",21,true,"-2018-06-06-"));
+    }//时间格式有误
+    @Test
+    public void withdraw13() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("1001","1621",21,true,"2018-06-06 24:00:00"));
+    }//小时错误
+    @Test
+    public void withdraw14() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("1001","1621",21,true,"2018-06-06 12:60:00"));
+    }//分错误
+    @Test
+    public void withdraw15() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("1001","1621",21,true,"2018-06-06 12:00:60"));
+    }//秒错误
+     @Test
+    public void withdraw16() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Withdraw("1001","1621",21,true,""));
+    }//时间空
+
+
+    //正确参数
+    @Test
+    public void withdrawA() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("1001",
+                css.Withdraw("1001","1621",21,true,"2018-05-30 00:00:00"));
+    }
+    @Test
+    public void withdrawB() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("1002",
+                css.Withdraw("1002","1621",21,true,"2018-06-30 00:00:00"));
+    }
+    @Test
+    public void withdrawC() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("1003",
+                css.Withdraw("1003","1621",21,true,"2018-02-28 00:00:00"));
+    }
+    @Test
+    public void withdrawD() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("1004",
+                css.Withdraw("1004","1621",21,true,"2008-02-29 00:00:00"));
+    }
+*/
+
+    /*trade*/
+    @Test
+    public void trade1() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("","1621","2333",99,"2018-06-06 01:00:30"));
+    }//requestID为空
+    @Test
+    public void trade2() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("abc","1621","2333",99,"2018-06-06 01:00:30"));
+    }//requestID错误
+    @Test
+    public void trade3() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","","2333",99,"2018-06-06 01:00:30"));
+    }//userID为空
+    @Test
+    public void trade4() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","abc","2333",99,"2018-06-06 01:00:30"));
+    }//userID错误
+    @Test
+    public void trade5() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","1621","",99,"2018-06-06 01:00:30"));
+    }//merchantID为空
+    @Test
+    public void trade6() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","1621","abc",99,"2018-06-06 01:00:30"));
+    }//merchantID错误
+    @Test
+    public void trade7() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","1621","2333",-100,"2018-06-06 01:00:30"));
+    }//amount为负
+    @Test
+    public void trade8() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","1621","2333",99,"2018-00-06 01:00:30"));
+    }//月错误1
+    @Test
+    public void trade9() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","1621","2333",99,"2018-14-06 01:00:30"));
+    }//月错误2
+    @Test
+    public void trade10() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","1621","2333",99,"2018-05-32 01:00:30"));
+    }//大月错误
+    @Test
+    public void trade11() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","1621","2333",99,"2018-06-31 01:00:30"));
+    }//小月错误
+    @Test
+    public void trade12() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","1621","2333",99,"2018-02-29 01:00:30"));
+    }//非闰年2月
+    @Test
+    public void trade13() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","1621","2333",99,"2018-02-30 01:00:30"));
+    }//闰年2月
+    @Test
+    public void trade14() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","1621","2333",99,""));
+    }//时间为空
+    @Test
+    public void trade15() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","1621","2333",99,"2018-06-06 24:00:30"));
+    }//时
+    @Test
+    public void trade16() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","1621","2333",99,"2018-06-06 01:60:00"));
+    }//分
+    @Test
+    public void trade17() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","1621","2333",99,"2018-06-06 01:00:60"));
+    }//秒
+    @Test
+    public void trade18() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("-1",
+                css.Trade("404","1621","2333",99,"asdfghjkl"));
+    }//时间格式错误
+
+    //有效等价类
+    @Test
+    public void tradeA() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("1001",
+                css.Trade("1001","1621","2333",66.6,"2018-06-06 01:00:30"));
+    }//
+    @Test
+    public void tradeB() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("1002",
+                css.Trade("1002","1621","2333",99.8,"2018-06-06 01:00:30"));
+    }//
+    @Test
+    public void tradeC() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("1003",
+                css.Trade("1003","1621","2333",66.6,"2018-06-06 01:00:30"));
+    }//
+    @Test
+    public void tradeD() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("1004",
+                css.Trade("1004","1621","2333",66.6,"2018-06-06 01:00:30"));
+    }//
+
+/**/
+/*
+    @Test
+    public void queryRecord1() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("[]",
+                css.QueryRecord("2018-06-01 00:00:00","2018-03-08 23:59:59",2));
+    }
+*/
+/*
+    @Test
+     public void downloadFile1() throws Exception{
+        CSSystemImpl css = new CSSystemImpl();
+        assertEquals("[]",
+                css.DownloadFile("2018-06-01 00:00:00"));
+    }
+*/
+}
